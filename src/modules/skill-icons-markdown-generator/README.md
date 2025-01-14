f6424802a1e2703a5197525e1559429a99755562
HallexCosta <hallex.costa@hotmail.com>
2025-01-14 13:04:59 -0300

    feat(skill-icons-markdown-generator): add new module to generate icons in table markdown format

---
+++ src/modules/skill-icons-markdown-generator/README.md
@@ -0,0 +1,3 @@
+## Overview
+
+Module responsiveness to control the logic and implementarion of service that generate a markdown table with all icons available :smiley:
---
+++ src/modules/skill-icons-markdown-generator/index.js
@@ -0,0 +1,31 @@
+import tablemark from 'tablemark'
+import { getSkillIconsDb } from '../shared/services/getSkillIconsDb.js'
+
+import fs from 'node:fs/promises'
+
+const html = String.raw
+
+const wrapperSvg = (svgs) =>
+  html`<div style="display: flex; align-items: center; justify-content: center; gap: 10px;">${svgs}</div>`
+
+const mapToSvgImg = (theme, id) =>
+  html`<img src="./icons/${theme}:${id}.svg" />`
+
+const mapToTablemark = (skillIcons) => {
+  return Object.keys(skillIcons).map((id) => ({
+    id,
+    svg: wrapperSvg(
+      skillIcons[id].themes.map((theme) => mapToSvgImg(theme, id)).join('')
+    ),
+    ...skillIcons[id]
+  }))
+}
+
+export const main = async () => {
+  const table = tablemark(mapToTablemark(await getSkillIconsDb()))
+  const dir = `${process.cwd()}/docs/ICONS_AVAILABLES.md`
+  fs.writeFile(dir, table)
+  console.log('Great! available on dir', dir)
+}
+
+main()
