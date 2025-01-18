11dae06026bc93b5eec4bc4496362fbad8bb9cb6
HallexCosta <hallex.costa@hotmail.com>
2025-01-17 21:07:57 -0300

    feat(skill-icons-markdown-generator): add total icons availables

--- src/modules/skill-icons-markdown-generator/index.js
+++ src/modules/skill-icons-markdown-generator/index.js
@@ -9,7 +9,7 @@ const wrapperSvg = (svgs) =>
   html`<div style="display: flex; align-items: center; justify-content: center; gap: 10px;">${svgs}</div>`
 
 const mapToSvgImg = (theme, id) =>
-  html`<img src="../icons/${theme}:${id}.svg" />`
+  html`<img width="52" height="52" src="../icons/${theme}:${id}.svg" />`
 
 const mapToTablemark = (skillIcons) => {
   return Object.keys(skillIcons).map((id) => ({
@@ -17,15 +17,19 @@ const mapToTablemark = (skillIcons) => {
     svg: wrapperSvg(
       skillIcons[id].themes.map((theme) => mapToSvgImg(theme, id)).join('')
     ),
-    ...skillIcons[id]
+    ...skillIcons[id],
+    total: '-'
   }))
 }
 
 export const main = async () => {
-  const table = tablemark(mapToTablemark(await getSkillIconsDb()))
+  const mappedSkillIcons = mapToTablemark(await getSkillIconsDb())
+  const table = tablemark(mappedSkillIcons)
   const dir = `${process.cwd()}/docs/ICONS_AVAILABLES.md`
-  fs.writeFile(dir, table)
-  console.log('Great! available on dir', dir)
+
+  const header = `# Total Icons: ${mappedSkillIcons.length}\n`
+  fs.writeFile(dir, header.concat(table))
+  console.log('Great! available on dir:', dir)
 }
 
 main()
